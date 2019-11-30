959452dd3c786dbb1557eeda645d60a14d80cbeb
Atmir Ataev <atmirataev@gmail.com>
2019-11-30 19:56:38 +0300

    Добавляет слайдер в блок "Отзывы"

--- build/js/comments.js
+++ build/js/comments.js
@@ -1,1 +1,25 @@
 'use strict';
+
+(function () {
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.comments__btn-next',
+      prevEl: '.comments__btn-prev',
+    },
+  });
+
+  var nextBtn = document.querySelector('.comments__btn-next');
+  var title = document.querySelector('.comments__title');
+
+  title.addEventListener('click', function () {
+    alert('clicked');
+  });
+
+  nextBtn.addEventListener('click', function () {
+    alert('clicked');
+  });
+})();
--- source/js/life-in-Israel.js
+++ source/js/life-in-Israel.js
@@ -1,30 +1,32 @@
 'use strict';
 
-var breakpoint = window.matchMedia('(min-width:768px)');
-var lifeInIsraelSwiper;
+(function () {
+  var breakpoint = window.matchMedia('(min-width:768px)');
+  var lifeInIsraelSwiper;
 
-var breakpointChecker = function () {
-  if (breakpoint.matches === true) {
+  var breakpointChecker = function () {
+    if (breakpoint.matches === true) {
 
-    if (lifeInIsraelSwiper !== undefined) {
-      lifeInIsraelSwiper.destroy(true, true);
+      if (lifeInIsraelSwiper !== undefined) {
+        lifeInIsraelSwiper.destroy(true, true);
+      }
+
+      return;
+    } else if (breakpoint.matches === false) {
+      return enableSwiper();
     }
+  };
 
-    return;
-  } else if (breakpoint.matches === false) {
-    return enableSwiper();
-  }
-};
-
-var enableSwiper = function () {
-  lifeInIsraelSwiper = new Swiper('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  });
-};
+  var enableSwiper = function () {
+    lifeInIsraelSwiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  };
 
 
-breakpoint.addListener(breakpointChecker);
+  breakpoint.addListener(breakpointChecker);
 
-breakpointChecker();
+  breakpointChecker();
+})();
